0828b885842bc24f74985af62e6d26e377a2f084
Diya Mittal <diyamittal5113@gmail.com>
2025-10-13 18:23:35 +0530

    Time: 1 ms (75.31%), Space: 41.4 MB (39.25%) - LeetHub

---
+++ 0504-base-7/0504-base-7.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String convertToBase7(int num) {
+        StringBuilder sb=new StringBuilder();
+        int n=num,s=num;
+        if(n<0) n=num+(num*-2);
+        if(n==0) return "0";
+        while(n!=0){
+            int rem=n%7;
+            sb.append(rem);
+            n=n/7;
+        }
+        sb.reverse();
+        if(num<0) sb.insert(0, "-");
+        return sb.toString();
+    }
+}
